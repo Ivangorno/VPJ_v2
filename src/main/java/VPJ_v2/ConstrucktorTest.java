0c589ec7c37771b29b1ee4165072d5ef9db5764f
Ivangorno <111599129+Ivangorno@users.noreply.github.com>
2022-09-15 14:51:50 +0300

    added an empty Constructor "ConstrucktorTest()"

--- src/main/java/VPJ_v2/ConstrucktorTest.java
+++ src/main/java/VPJ_v2/ConstrucktorTest.java
@@ -3,24 +3,28 @@ package VPJ_v2;
 public class ConstrucktorTest {
 
     int i;
-
-
+    int x;
+    String string;
       private ConstrucktorTest() {}
 
      private   ConstrucktorTest(int number){
 
        this.i = number;
+    }
+       ConstrucktorTest(int number1, String s){
 
+        this.i = number1;
+        this.string = s;
     }
 
 
     public static void main(String[] args) {
 
+        ConstrucktorTest c1 = new ConstrucktorTest(8,"seven");
+        ConstrucktorTest c2 = new ConstrucktorTest(6);
+        ConstrucktorTest c3 = new ConstrucktorTest();
 
-        ConstrucktorTest c1 = new ConstrucktorTest(7);
-
-        System.out.println("number is = "+c1.i);
-
-
+        System.out.println("Number in object one is = "+c1.i+" and "+c1.string);
+        System.out.println("Number in object two is = "+c2.i);
     }
 }
